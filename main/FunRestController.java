55313e26fc1e6a12e75f8cd8c3caaf6f18fba94d
Eliza-Maria Ciuc <eliza.maria.c.2006@gmail.com>
2026-07-14 14:59:59 +0300

    Update FunRestController.java
    Injecting properties

--- main/FunRestController.java
+++ main/FunRestController.java
@@ -1,10 +1,24 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    //inject properties for coach.name, coach.team
+    @Value("${coach.name}")
+    private String coach_name;
+    @Value("${team.name}")
+    private String team_name;
+
+    //expose new endpoint for "teaminfo"
+    @GetMapping("/teaminfo")
+    public String get_team_info() {
+        //it will be replaced with Eliza , and Borussia Dortmund
+        return ("{ Coach name : " + coach_name + ", Team name : " + team_name + " }");
+    }
+
     //expose "/" that returns hello world
     @GetMapping("/")
     public String sayHello() {
