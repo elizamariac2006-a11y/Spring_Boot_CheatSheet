0f328050daea8643bdcc9655e9bc9e78013ed198
Eliza-Maria Ciuc <eliza.maria.c.2006@gmail.com>
2026-07-20 16:03:45 +0300

    Update CricketCoach.java - @PostConstruct and @PreDestroy

--- spring-core/dependency-injection/CricketCoach.java
+++ spring-core/dependency-injection/CricketCoach.java
@@ -1,11 +1,11 @@
 package com.luv2code.springcoredome.common;
 
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Scope;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //Now Spring creates two different object in memory2 beans)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //Now Spring creates two different object in memory2 beans)
 // by default its singleton - only one object is created and shared.
 public class CricketCoach implements Coach {
     public CricketCoach() {
@@ -15,5 +15,37 @@ public class CricketCoach implements Coach {
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15  minutes!!!!";
+    }
+
+    @PostConstruct
+    //I can have more, but I dont know how they will act... so its better to have one that manages
+    //the rest in case they have a logical order..
+    public void afterInitialization() {
+        firstFuction();
+        secondFunction();
+    }
+
+    private void firstFuction() {
+        System.out.println("First part after constructor...");
+    }
+
+    private void secondFunction() {
+        System.out.println("And now second part after constructor..");
+    }
+
+    @PreDestroy
+    //Same logic, we dont know in which order they are called, so its better to have one..
+    //FOR BEANS OF TYPE PROTOTYPE THE @PreDestroy IS NOT CALLED!!!!
+    public void beforeDestructor() {
+        functionBeforeDestruction1();
+        functionBeforeDestruction2();
+    }
+
+    private void functionBeforeDestruction1() {
+        System.out.println("The object will be destroyed...");
+    }
+
+    private void functionBeforeDestruction2() {
+        System.out.println("Right after this message...");
     }
 }
