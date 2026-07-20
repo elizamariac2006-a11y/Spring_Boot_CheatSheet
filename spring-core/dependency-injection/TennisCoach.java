85015889c3cbe0bcd779d883ec378b189011c71f
Eliza-Maria Ciuc <eliza.maria.c.2006@gmail.com>
2026-07-20 14:36:51 +0300

    Update TennisCoach.java

--- spring-core/dependency-injection/TennisCoach.java
+++ spring-core/dependency-injection/TennisCoach.java
@@ -3,6 +3,7 @@ package com.luv2code.springcoredome.common;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class TennisCoach implements Coach {
     @Override
     public String getDailyWorkout() {
