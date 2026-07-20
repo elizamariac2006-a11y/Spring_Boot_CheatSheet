ef9c0f77435bb1d99300d9122c80534cb4d89a6d
Eliza-Maria Ciuc <eliza.maria.c.2006@gmail.com>
2026-07-20 15:47:17 +0300

    Update CricketCoach.java - Added Bean Scopes

--- spring-core/dependency-injection/CricketCoach.java
+++ spring-core/dependency-injection/CricketCoach.java
@@ -1,9 +1,17 @@
-package com.luv2code.springcoredome;
+package com.luv2code.springcoredome.common;
 
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //Now Spring creates two different object in memory2 beans)
+// by default its singleton - only one object is created and shared.
 public class CricketCoach implements Coach {
+    public CricketCoach() {
+        System.out.println("In constructor : " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Practice fast bowling for 15  minutes!!!!";
