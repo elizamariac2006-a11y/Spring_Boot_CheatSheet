f94ddc6d1625bcfad1099107d0d43a6a95fcd293
Eliza-Maria Ciuc <eliza.maria.c.2006@gmail.com>
2026-07-20 14:10:42 +0300

    Update DemoController.java - added @Qualifier

--- spring-core/dependency-injection/DemoController.java
+++ spring-core/dependency-injection/DemoController.java
@@ -2,6 +2,7 @@ package com.luv2code.springcoredome.rest;
 
 import com.luv2code.springcoredome.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,17 +12,20 @@ public class DemoController {
     //define a private field for the dependency
     private Coach myCoach;
 
-    //define a constructor for dependency injection
-//    @Autowired
-//    public DemoController(Coach theCoach) {
-//        this.myCoach = theCoach;
-//    }
-
-    //define a setter for dependency injection
+//    define a constructor for dependency injection
     @Autowired
-    public void setCoach(Coach theCoach) {
+    public DemoController(@Qualifier("tennisCoach") Coach theCoach) {
+        //we used @Qualifier to tell Spring which implementation we want
+        //(Spring has 4 Beans from the Coach interface)
+        //We use the BEAN ID - NAME OF THE CLASS WITH THE FIRST LETTER LOWER
         this.myCoach = theCoach;
     }
+
+    //define a setter for dependency injection
+//    @Autowired
+//    public void setCoach(Coach theCoach) {
+//        this.myCoach = theCoach;
+//    }
 
     @GetMapping("/daily-workout")
     public String getDailyWorkout() {
