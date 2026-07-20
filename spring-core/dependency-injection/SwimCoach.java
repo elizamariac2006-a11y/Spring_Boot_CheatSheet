df9b906234c501fda55ea3f3def2ac9aee08f7dd
Eliza-Maria Ciuc <eliza.maria.c.2006@gmail.com>
2026-07-20 17:40:49 +0300

    Create SwimCoach.java - @Bean annotation

---
+++ spring-core/dependency-injection/SwimCoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springcoredome.common;
+
+//not using the @Component annotation
+public class SwimCoach implements Coach {
+    public SwimCoach() {
+        System.out.println("In constructor : " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Swim 1000 meters as a warm up";
+    }
+}
